0301a7590fefc1902271a8a1517b537cabaafe5a
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-10-27 16:19:12 -0300

    feat:Now is possible login at the apk using google

--- app/src/main/java/com/aduilio/mytasks/activity/LoginActivity.kt
+++ app/src/main/java/com/aduilio/mytasks/activity/LoginActivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.aduilio.mytasks.R
 import com.aduilio.mytasks.databinding.ActivityLoginBinding
+import com.aduilio.mytasks.entity.Task
 import com.aduilio.mytasks.extension.value
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +30,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initComponents()
+        configureGoogleSignIn()
+    }
+
+    private fun configureGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     private fun initComponents() {
@@ -30,6 +50,42 @@ class LoginActivity : AppCompatActivity() {
         binding.btCreateAccount.setOnClickListener {
             createAccount()
         }
+
+        binding.btLoginWithGoogle.setOnClickListener{
+            loginWithGoogle()
+        }
+    }
+
+    private fun loginWithGoogle() {
+        val intent = googleSignInClient.signInIntent
+        googleLauncher.launch(intent)
+    }
+
+    private val googleLauncher = registerForActivityResult(
+    ActivityResultContracts.StartActivityForResult()
+    ) { activityResult ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(activityResult.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Não foi possível login com o google", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        Firebase.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val mainIntent = Intent(this, MainActivity::class.java)
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(mainIntent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Não foi possível autenticar com o firebase.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     private fun login() {
